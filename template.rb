f1982b660977721b88d298e79a43656294e5a2de
Marc S. Brooks <marc@mbrooks.info>
2024-05-29 12:15:50 -0700

    Added application template (:development)

---
+++ template.rb
@@ -0,0 +1,11 @@
+#
+# Custom application template.
+#
+# See: https://guides.rubyonrails.org/rails_application_templates.html
+#
+gem_group :development, :test do
+  gem 'rspec'
+  gem 'rubocop'
+  gem 'sorbet'
+  gem 'sorbet-runtime'
+end
